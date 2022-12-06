520ebf0840317337b67dc49f835401e2117149f8
aguilarklyno <aguilarklyno@gmail.com>
2022-12-06 14:33:48 +0900

    各ページの遷移後編集

--- my-app/src/Components/Pages/03_Education/Education.jsx
+++ my-app/src/Components/Pages/03_Education/Education.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Top_info from '../../Global_components/Top_info'
+import Hero from '../../Global_components/Hero_components'
+import Title from '../../Global_components/Title_components'
+import Box from '../../Global_components/Box_components'
+
+// ---------------Images-------------------
+import Loading from "../../../images/Loading.png";
 
 const Education = () => {
   return (
-    <div>Education</div>
+    <>
+      <Top_info />
+      <Hero type={"Education"} span={"-教育-"}/>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Hospital"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/Education/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+    </>
   )
 }
-
 export default Education
--- my-app/src/Components/Pages/08_DailyLife/DailyLife.jsx
+++ my-app/src/Components/Pages/08_DailyLife/DailyLife.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Top_info from '../../Global_components/Top_info'
+import Hero from '../../Global_components/Hero_components'
+import Title from '../../Global_components/Title_components'
+import Box from '../../Global_components/Box_components'
+
+// ---------------Images-------------------
+import Loading from "../../../images/Loading.png";
 
 const DailyLife = () => {
   return (
-    <div>DailyLife</div>
+    <>
+      <Top_info />
+      <Hero type={"DailyLife"} span={"-日常生活-"}/>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+      <Title type={"Loading"}/>
+        <div className='flex flex-wrap justify-center items-center'>
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+          <Box to={"/DailyLife/none"} type={"Loading"} src={Loading} alt={"Loading"} />
+        </div>
+    </>
   )
 }
-
 export default DailyLife
