60fa4c2f5534cb9f84855c6ce9acede7baba97be
aguilarklyno <aguilarklyno@gmail.com>
2022-12-06 12:53:39 +0900

    ページの遷移：SupportCell/List周りの整備

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,17 @@ import "./CSS/App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home/Home";
+/* -----------------Information---------------------- */
+import Medical from "./Components/Pages/Medical";
+import Raising from "./Components/Pages/Raising";
+import Education from "./Components/Pages/Education";
+import Emergency from "./Components/Pages/Emergency";
+import Job from "./Components/Pages/Job";
+import Transportation from "./Components/Pages/Transportation";
+import Tax from "./Components/Pages/Tax";
+import DailyLife from "./Components/Pages/DailyLife";
+
+
 
 function App() {
   return (
@@ -13,6 +24,17 @@ function App() {
         <Header />
           <Routes>
             <Route path="/" element={<Home />}/>
+{/* -----------------Information---------------------- */}
+            <Route path="/medical" element={<Medical />}/>
+            <Route path="/raising" element={<Raising/>}/>
+            <Route path="/education" element={<Education/>}/>
+            <Route path="/emergency" element={<Emergency/>}/>
+            <Route path="/job" element={<Job/>}/>
+            <Route path="/transportation" element={<Transportation/>}/>
+            <Route path="/tax" element={<Tax/>}/>
+            <Route path="/dailylife" element={<DailyLife/>}/>
+            
+            
           </Routes>
         <Footer />
       </div>
